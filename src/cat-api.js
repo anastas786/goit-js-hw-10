f4ca09af8596c39c513c9e6036604a52c43c5814
anastas786 <agorbatuk08@gmail.com>
2023-10-05 17:52:44 +0200

    features 1,2

---
+++ src/cat-api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+// Налаштування заголовку x-api-key
+axios.defaults.headers.common["x-api-key"] = live_KRGK14iZREdwtEGhSnQsBkuiz58h0w8qAZZgn6jKPJGgKxBlnwB42Xn50Fc0oquv;
+
+
+// Функція для виконання HTTP-запиту та отримання списку порід
+export function fetchBreeds() {
+    // URL для запиту до API The Cat API
+    const apiUrl = "https://api.thecatapi.com/v1/breeds";
+
+    // Виконання GET-запиту до API
+    return axios.get(apiUrl)
+        .then(response => {
+            // Отримання даних про породи з відповіді API
+            const breeds = response.data;
+            return breeds;
+        })
+        .catch(error => {
+            throw error; // Обробка помилок у виклику коду, який викликав цю функцію
+        });
+
+}
+
+// // Функція для виконання HTTP-запиту та отримання даних про кота за ідентифікатором породи
+// export function fetchCatByBreed(breedId) {
+//     // URL для запиту до API The Cat API з параметром breed_ids
+//     const apiUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+
+//     // Виконання GET-запиту до API
+//     return axios.get(apiUrl)
+//         .then(response => {
+//             // Отримання даних про кота з відповіді API
+//             const catData = response.data[0]; // Беремо перший об'єкт з масиву, якщо він є
+//             return catData;
+//         })
+//         .catch(error => {
+//             throw error; // Обробка помилок у виклику коду, який викликав цю функцію
+//         });
+// }
+
+
